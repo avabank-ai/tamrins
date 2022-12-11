e6ce97232f697bd37fc7b5f15f83233d402771d0
avabank-ai <avabankteam@gmail.com>
2022-12-11 09:02:40 +0330

    first upload

---
+++ tamrin_1/number_of_n_words.py
@@ -0,0 +1,19 @@
+
+
+def word_in_sentence(sent, n):
+    number_of_n = 0
+    list_words = []
+    words = sent.split()
+    for w in words:
+        if len(w) == n:
+            number_of_n += 1
+            list_words.append(w)
+
+    print("number of words with len ",n,"= ",number_of_n)
+    print("words are: ", list_words)
+
+sentence = input("type your sent ")
+number = int(input("enter number "))
+
+
+word_in_sentence(sent=sentence, n=number)
